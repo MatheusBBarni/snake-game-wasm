868a31e7ab405697ecce3a4333c0425f5ec62dd0
MatheusBBarni <brehm.matheus@hotmail.com>
2022-07-06 14:37:56 -0300

    wip

--- snake_game_wasm/src/lib.rs
+++ snake_game_wasm/src/lib.rs
@@ -1,16 +1,93 @@
 use wasm_bindgen::prelude::*;
+use wee_alloc::WeeAlloc;
 
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(name)
+#[global_allocator]
+static ALLOC: WeeAlloc = WeeAlloc::INIT;
+
+pub enum Direction {
+    Up,
+    Right,
+    Down,
+    Left
 }
 
 #[wasm_bindgen]
-extern {
-    pub fn alert(msg: &str);
+pub struct World {
+    width: usize,
+    size: usize,
+    snake: Snake
+}
+
+struct SnakeCell(usize);
+
+struct Snake {
+    body: Vec<SnakeCell>,
+    direction: Direction
+}
+
+impl Snake {
+    pub fn new(spawn_index: usize) -> Snake {
+        Snake {
+            body: vec!(SnakeCell(spawn_index)),
+            direction: Direction::Left
+        }
+    }
 }
 
 #[wasm_bindgen]
-pub fn sum(num1: i32, num2: i32) -> i32 {
-    num1 + num2
+impl World {
+    pub fn new(width: usize, snake_index: usize) -> World {
+        World {
+            width,
+            size: width * width, 
+            snake: Snake::new(snake_index)
+        }
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
+
+    pub fn snake_head(&self) -> usize {
+        self.snake.body[0].0
+    }
+
+    pub fn update(&mut self) {
+        let snake_index = self.snake_head();
+        let world_width = self.width();
+
+        let (row, col) = self.index_to_cell(snake_index);
+
+        let (row, col) = match self.snake.direction {
+            Direction::Right => {
+                (row, (col + 1) % world_width)
+            },
+            Direction::Left => {
+                (row, (col - 1) % world_width)
+            },
+            Direction::Up => {
+                ((row - 1) % world_width, col)
+            },
+            Direction::Down => {
+                ((row + 1) % world_width, col)
+            },
+        };
+
+        self.set_snake_head(self.cell_to_index(row, col));
+    }
+
+    fn set_snake_head(&mut self, index: usize) {
+        self.snake.body[0].0 = index;
+    }
+
+    fn index_to_cell(&self, index: usize) -> (usize, usize) {
+        let row = index / self.width();
+        let col = index % self.width();
+
+        (row, col)
+    }
+
+    fn cell_to_index(&self, row: usize, col: usize) -> usize {
+        (row * self.width()) + col
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,70 @@
-import init, { greet, sum } from 'snake_game_wasm'
+import './styles.css'
 
-async function main() {
+import init, { World } from 'snake_game_wasm'
+
+const canvas = document.querySelector(".wrapper #snake-screen") as HTMLCanvasElement
+
+(async () => {
   await init()
 
+  const WORLD_WIDTH = 16;
+  const SNAKE_INDEX = Date.now() % (WORLD_WIDTH * WORLD_WIDTH);
 
-  console.log('init wasm-pack')
-  greet('from vite!')
-  console.log("sum from wasm", sum(2, 2))
-}
+  const world = World.new(WORLD_WIDTH, SNAKE_INDEX)
 
-main()
+  const context = canvas.getContext("2d") as CanvasRenderingContext2D
+  const CELL_SIZE = 10
+
+  const worldWidth = world.width()
+  const worldSize = worldWidth * CELL_SIZE
+
+  canvas.height = worldSize
+  canvas.width = worldSize
+
+  function drawWorld() {
+    context.beginPath()
+
+    for (let x = 0; x < worldWidth + 1; x++) {
+      context.moveTo(CELL_SIZE * x, 0)
+      context.lineTo(CELL_SIZE * x, worldSize)
+    }
+    for (let y = 0; y < worldWidth + 1; y++) {
+      context.moveTo(0, CELL_SIZE * y)
+      context.lineTo(worldSize, CELL_SIZE * y)
+    }
+
+    context.stroke()
+  }
+
+  function drawSnake() {
+    const snakeIndex = world.snake_head()
+    const col = snakeIndex % worldWidth
+    const row = Math.floor(snakeIndex / worldWidth)
+
+    context.beginPath()
+
+    context.fillRect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+
+    context.stroke()
+  }
+
+  function paint() {
+    drawWorld()
+    drawSnake()
+  }
+
+  function update() {
+    const FPS = 5;
+    setTimeout(() => {
+      context.clearRect(0, 0, canvas.width, canvas.height)
+      world.update()
+      paint()
+      requestAnimationFrame(update)
+    }, 1000 / FPS)
+  }
+  
+  paint()
+  update()
+
+})()
+
